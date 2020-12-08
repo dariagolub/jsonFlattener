42b1e59427762f5ed54b4c7a6e6e51f3da7d565f
Daria Golub <darya.jukova@gmail.com>
2020-12-08 17:07:16 +0100

    flatten json

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -9,14 +10,13 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter json:")
 
 	var stringBuilder strings.Builder
 
 	for {
 		line, _ := reader.ReadString('\n')
 
-		if len(line) ==0 {
+		if len(line) == 0 {
 			break
 		}
 
@@ -24,5 +24,34 @@ func main() {
 
 	}
 
-	fmt.Printf(stringBuilder.String())
+	rawData := []byte(stringBuilder.String())
+
+	var inputJson map[string]interface{}
+	err := json.Unmarshal(rawData, &inputJson)
+	
+	if err != nil {
+		panic(err) //todo
+	}
+
+	var leftKey = ""
+	keyDelimiter := "."
+	var flattenedJson = make(map[string]interface{})
+
+	flattenJson(inputJson, &flattenedJson, leftKey, keyDelimiter)
+	for key, value := range flattenedJson {
+		fmt.Printf("%v:%v\n", key, value)
+	}
+
+}
+
+func flattenJson(inputJSON map[string]interface{}, flattened *map[string]interface{}, leftKey string, keyDelimiter string) {
+	for key, value := range inputJSON {
+		key := leftKey + key
+
+		if _, ok := value.(map[string]interface{}); ok {
+			flattenJson(value.(map[string]interface{}), flattened, key+keyDelimiter, keyDelimiter)
+		} else {
+			(*flattened)[key] = value
+		}
+	}
 }
